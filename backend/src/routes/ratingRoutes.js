ed2b8d5ee512eb9220d016531bacee1d8fd01dc3
Nayessha Marya <nayesshamarya@gmail.com>
2026-02-27 02:35:46 +0530

    made changes

--- backend/src/routes/ratingRoutes.js
+++ backend/src/routes/ratingRoutes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 const verifyToken = require("../middleware/verifyToken");
 const { rateUser } = require("../controllers/ratingController");
+const { addRating } = require("../controllers/ratingController");
+
 
 router.post("/", verifyToken, rateUser);
 
 module.exports = router;
+
+
+
