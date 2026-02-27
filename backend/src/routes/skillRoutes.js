956510de4e434d1ea19c1f0d81692e4dcdd19bdd
Nayessha Marya <nayesshamarya@gmail.com>
2026-02-27 20:54:56 +0530

    updated

--- backend/src/routes/skillRoutes.js
+++ backend/src/routes/skillRoutes.js
@@ -9,16 +9,18 @@ const {
   addWantedSkill,
   getSkillsByCategory,
   getMySkills,
-  getMyWantedSkills   // ← add this
+  getMyWantedSkills,
+  getRecommendedTeachers    
 } = require("../controllers/skillController");
 
 router.post("/add", verifyToken, addSkill);
 router.get("/", getAllSkills);
 router.post("/wanted", verifyToken, addWantedSkill);
-router.get("/recommend", verifyToken, getRecommendedTeachers);
+
 
 router.get("/by-category", getSkillsByCategory);
 router.get("/my", verifyToken, getMySkills);
 router.get("/wanted", verifyToken, getMyWantedSkills);
+router.get("/recommend", verifyToken, getRecommendedTeachers);
 
 module.exports = router;
